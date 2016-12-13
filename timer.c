5be0aaabde43bb31f721032828b70aa18845d5ec
root <root@linux-yg6k.site>
2016-12-13 08:49:02 +0100

    make it more beautiful

--- timer.c
+++ timer.c
@@ -18,71 +18,21 @@
 
 #include "timer.h"          
 
-#define  DEVICE_NAME_1 "timerf"    ///< The device will appear at /dev/timerf using this value
-#define  DEVICE_NAME_2 "timerr"    ///< The device will appear at /dev/timerr using this value
-#define  CLASS_NAME  "timer"        ///< The device class -- this is a character device driver
-
 MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
 MODULE_AUTHOR("Florian Heuer");    ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("Timer Device");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");            ///< A version number to inform users
 
-static int    majorNumber;                  ///< Stores the device number -- determined automatically
-static int    minorNumber = 0; 
-static char   message[256];           ///< Memory for the string that is passed from userspace
-static short  size_of_message;              ///< Used to remember the size of the string stored
-       int    numberOpens = 0;              ///< Counts the number of times the device is opened
-static struct class*  timerClass  = NULL; ///< The device-driver class struct pointer
-static struct device* timerDevice = NULL; ///< The device-driver device struct pointer
-
-char cmd_char;
-unsigned long cmd_value;
-int send;
-struct state_machine *current_device;
-u64 jiffies_pause_start = 0;
-
-#define NUM_MACHINES 2
-
-#define RDY   0
-#define LOAD  2
-#define RUN   4
-#define PAUSE 8
-
-struct state_machine{
-   char *name;
-   dev_t id;
-   int state;
-   u64 start_jiffies;
-   u64 pause_jiffies;
-   int loaded_value;
-   int (*read) (const char*);
-   void (*write) (void);
-};
-
-struct state_machine machines[NUM_MACHINES];
-
-int register_device(char* name);
-int register_device_major_number(void);
-
-int read_timerf(const char *buffer);
-int read_timerr(const char *buffer);
-
-void write_timerf(void);
-void write_timerr(void);
-
-int jiffies_to_seconds(long jiffies);
-
 int 
 register_device_major_number(){
-   printk(KERN_INFO "TIMER: Initializing %s on LKM\n", CLASS_NAME);
 
    // Try to dynamically allocate a major number for the device -- more difficult but worth it
    majorNumber = register_chrdev(0, CLASS_NAME, &fops);
    if (majorNumber<0){
-      printk(KERN_ALERT "%s failed to register a major number\n", CLASS_NAME);
+      PDEBUG("%s failed to register a major number\n", CLASS_NAME);
       return majorNumber;
    }
-   printk(KERN_INFO "TIMER: registered correctly with major number %d\n", majorNumber);
+   PDEBUG("registered correctly with major number %d\n", majorNumber);
 
    return 0;
 }
@@ -97,10 +47,10 @@ register_device(char* name){
    if (IS_ERR(timerDevice)){               // Clean up if there is an error
       class_destroy(timerClass);           // Repeated code but the alternative is goto statements
       unregister_chrdev(majorNumber, name);
-      printk(KERN_ALERT "Failed to create the device\n");
+      PDEBUG("Failed to create the device\n");
       return PTR_ERR(timerDevice);
    }
-   printk(KERN_INFO "TIMER: device(%s) created correctly\n", name); // Made it! device was initialized
+   PDEBUG("TIMER: device(%s) created correctly\n", name);
 
    //set device attributes
    machines[minorNumber].name  = name;
@@ -124,17 +74,16 @@ register_device(char* name){
  */
 static int __init timer_init(void){
 
-   //init state machines
-   //machines[0] = kmalloc(size_of(state_machine));
-   //machines[1] = kmalloc(size_of(state_machine));
+   PDEBUG("Initializing %s on LKM\n", CLASS_NAME);
 
    // Register the device class
    timerClass = class_create(THIS_MODULE, CLASS_NAME);
    if (IS_ERR(timerClass)){                // Check for error and clean up if there is
-      printk(KERN_ALERT "Failed to register device class\n");
+      PDEBUG("Failed to register device class\n");
+
       return PTR_ERR(timerClass);          // Correct way to return an error on a pointer
    }
-   printk(KERN_INFO "TIMER: device class registered correctly\n");
+   PDEBUG("TIMER: device class registered correctly\n");
 
    register_device_major_number();
 
@@ -154,7 +103,7 @@ static void __exit timer_exit(void){
    class_unregister(timerClass);                          // unregister the device class
    class_destroy(timerClass);                             // remove the device class
    unregister_chrdev(majorNumber, CLASS_NAME);             // unregister the major number
-   printk(KERN_INFO "TIMER: Goodbye from the LKM!\n");
+   PDEBUG("TIMER: Goodbye from the LKM!\n");
 }
 
 /** @brief The device open function that is called each time the device is opened
@@ -163,7 +112,6 @@ static void __exit timer_exit(void){
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_open(struct inode *inodep, struct file *filep){
-   numberOpens++;
    send=0;
 
    //map current device
@@ -174,7 +122,7 @@ static int dev_open(struct inode *inodep, struct file *filep){
       }
    }
 
-   printk(KERN_INFO "TIMER: Device has been (%s) opened\n", current_device->name);
+   PDEBUG("TIMER: Device has been (%s) opened\n", current_device->name);
    return 0;
 }
 
@@ -218,7 +166,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 
    current_device->write();
 
-   printk(KERN_INFO "TIMER: Received (%s) from the user! (%c:%d)\n", copy, cmd_char, cmd_value);
+   PDEBUG("TIMER: Received (%s) from the user! (%c:%d)\n", copy, cmd_char, cmd_value);
 
    memset(buffer, 0x00, sizeof(buffer));   /* clear buffer */;
    return len;
@@ -230,32 +178,12 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_release(struct inode *inodep, struct file *filep){
-   printk(KERN_INFO "TIMER: Device successfully closed\n");
+   PDEBUG("TIMER: Device successfully closed\n");
    return 0;
 }
 
-
-
-
-//transitions
-#define START_TIMER     's'
-#define RESET_TIMER     'r'
-#define LOAD_TIMER      'l'
-#define PAUSE_TIMER     'p'
-#define CONTINUE_TIMER  'c'
-
-
-//=====
-void on_ready_timerf(void);
-void on_ready_timerr(void);
-void on_load(void);
-void on_pause(void);
-void on_run(void);
-
-
 void
 write_timerf(){
-  printk(KERN_INFO "TIMER: enter write of %s state is (%d)", current_device->name, current_device->state);
    switch(current_device->state){
       case RDY: 
          on_ready_timerf();
@@ -271,7 +199,6 @@ write_timerf(){
 
 void
 write_timerr(){
-   
    switch(current_device->state){
       case RDY: 
          on_ready_timerr();
@@ -288,18 +215,14 @@ write_timerr(){
    } 
 }
 
-
-
-
-
-
 void 
 on_ready_timerf(){
    switch(cmd_char){
       case START_TIMER:
          current_device->start_jiffies = get_jiffies_64();
          current_device->state = RUN;
-         printk(KERN_INFO "TIMER: start timer\n");
+         //printk(KERN_INFO "TIMER: start timer\n");
+         PDEBUG("start timer");
       break;
       default:
       break;
@@ -312,7 +235,7 @@ on_ready_timerr(){
       case LOAD_TIMER:
          current_device->loaded_value = cmd_value;
          current_device->state = LOAD;
-         printk(KERN_INFO "TIMER: load timer\n");
+         PDEBUG(KERN_INFO "TIMER: load timer\n");
       break;
       default:
       break;
@@ -327,14 +250,14 @@ on_pause(void){
          current_device->pause_jiffies += get_jiffies_64() - jiffies_pause_start;
          jiffies_pause_start=0;
          current_device->state = RUN;
-         printk(KERN_INFO "TIMER: continue timer\n");
+         PDEBUG(KERN_INFO "TIMER: continue timer\n");
       break;
       case RESET_TIMER:
          current_device->start_jiffies = 0;
          current_device->state = RDY;
          current_device->pause_jiffies = 0;
          jiffies_pause_start = 0;
-         printk(KERN_INFO "TIMER: reset timer\n");
+         PDEBUG(KERN_INFO "TIMER: reset timer\n");
       break;
       default:
       break;
@@ -347,14 +270,14 @@ on_run(void){
       case PAUSE_TIMER:
          jiffies_pause_start = get_jiffies_64();
          current_device->state = PAUSE;
-         printk(KERN_INFO "TIMER: pause timer\n");
+         PDEBUG(KERN_INFO "TIMER: pause timer\n");
       break;
       case RESET_TIMER:
          current_device->start_jiffies = 0;
          current_device->state = RDY;
          current_device->pause_jiffies = 0;
          jiffies_pause_start = 0;
-         printk(KERN_INFO "TIMER: reset timer\n");
+         PDEBUG(KERN_INFO "TIMER: reset timer\n");
       break;
       default:
       break;
@@ -367,29 +290,19 @@ on_load(void){
       case START_TIMER:
          current_device->start_jiffies = get_jiffies_64();
          current_device->state = RUN;
-         printk(KERN_INFO "TIMER: start timer\n");
+         PDEBUG(KERN_INFO "TIMER: start timer\n");
       break;
       case RESET_TIMER:
          current_device->start_jiffies = 0;
          current_device->state = RDY;
          current_device->pause_jiffies = 0;
          jiffies_pause_start = 0;
-         printk(KERN_INFO "TIMER: reset timer\n");
+         PDEBUG(KERN_INFO "TIMER: reset timer\n");
       break;
       default:
       break;
    }
 };
-
-
-
-
-
-
-static const char ready_state[]  = "READY"; 
-static const char run_state[]    = "RUNNING"; 
-static const char load_state[]   = "LOADED"; 
-static const char pause_state[]  = "PAUSED"; 
 
 char* get_state(int state){
    switch(state){
@@ -408,8 +321,6 @@ char* get_state(int state){
    }
    return "null";
 }
-
-
 
 int
 read_timerf(const char *buffer){
@@ -460,6 +371,8 @@ int
 jiffies_to_seconds(long jiffies){
    return jiffies/HZ;
 }
+
+
 
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
  *  identify the initialization function at insertion time and the cleanup function (as
--- timer.h
+++ timer.h
@@ -1,3 +1,43 @@
+//states
+#define NUM_MACHINES 2
+#define RDY   0
+#define LOAD  2
+#define RUN   4
+#define PAUSE 8
+
+//transitions
+#define START_TIMER     's'
+#define RESET_TIMER     'r'
+#define LOAD_TIMER      'l'
+#define PAUSE_TIMER     'p'
+#define CONTINUE_TIMER  'c'
+
+//names
+#define  DEVICE_NAME_1 "timerf"    ///< The device will appear at /dev/timerf using this value
+#define  DEVICE_NAME_2 "timerr"    ///< The device will appear at /dev/timerr using this value
+#define  CLASS_NAME  "timer"        ///< The device class -- this is a character device driver
+
+//State Strings
+static const char ready_state[]  = "READY"; 
+static const char run_state[]    = "RUNNING"; 
+static const char load_state[]   = "LOADED"; 
+static const char pause_state[]  = "PAUSED"; 
+
+//prototypes
+int register_device(char* name);
+int register_device_major_number(void);
+int read_timerf(const char *buffer);
+int read_timerr(const char *buffer);
+void write_timerf(void);
+void write_timerr(void);
+int jiffies_to_seconds(long jiffies);
+void on_ready_timerf(void);
+void on_ready_timerr(void);
+void on_load(void);
+void on_pause(void);
+void on_run(void);
+
+
 // The prototype functions for the character driver -- must come before the struct definition
 static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
@@ -15,3 +55,43 @@ static struct file_operations fops =
    .write = dev_write,
    .release = dev_release,
 };
+
+struct state_machine{
+   char *name;
+   dev_t id;
+   int state;
+   u64 start_jiffies;
+   u64 pause_jiffies;
+   int loaded_value;
+   int (*read) (const char*);
+   void (*write) (void);
+};
+
+//vars
+static int    majorNumber;                  ///< Stores the device number -- determined automatically
+static int    minorNumber = 0; 
+static char   message[256];           ///< Memory for the string that is passed from userspace
+static short  size_of_message;              ///< Used to remember the size of the string stored
+static struct class*  timerClass  = NULL; ///< The device-driver class struct pointer
+static struct device* timerDevice = NULL; ///< The device-driver device struct pointer
+
+char cmd_char;
+unsigned long cmd_value;
+int send;
+struct state_machine *current_device;
+u64 jiffies_pause_start = 0;
+struct state_machine machines[NUM_MACHINES];
+
+//MACROS
+#undef PDEBUG             /* undef it, just in case */
+#ifdef DEBUG
+#  ifdef __KERNEL__
+     /* This one if debugging is on, and kernel space */
+#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "TIMER: " fmt, ## args)
+#  else
+     /* This one for user space */
+#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
+#  endif
+#else
+#  define PDEBUG(fmt, args...) /* not debugging: nothing */
+#endif
